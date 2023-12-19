1fc09c67cca074a1283b4bd199da93576e6892fe
Daniel Cikora <d.cikora98@gmail.com>
2023-12-19 18:10:34 +0100

    Enviroment good, SCSS using cmd, nav and hero section started.

--- src/components/navigation/NavigationTop.jsx
+++ src/components/navigation/NavigationTop.jsx
@@ -3,13 +3,13 @@ export default function NavigationTop() {
   return (
     <nav className="navigation-top">
       <div className='wrapper navigation-top__wrapper'>
-        <a className="main-link navigation-top__logo" href="#">MAIN LOGO</a>
+        <a className="navigation-top__logo" href="#">DANIEL CIKORA</a>
         <ul className="navigation-top__ul">
           <li className="navigation-top__li">
-            <a className="navigation-top__a" href='#'>COLLECTIONS</a>
+            <a className="main-link navigation-top__a" href='#'>COLLECTIONS</a>
           </li>
           <li>
-            <a className="navigation-top__a" href='#'>EXPLORE</a>
+            <a className="main-link navigation-top__a" href='#'>EXPLORE</a>
           </li>
         </ul>
       </div>
