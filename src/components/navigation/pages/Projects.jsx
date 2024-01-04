595d6a600910da233d6117985cedf7a0588c670e
Daniel Cikora <d.cikora98@gmail.com>
2024-01-04 15:00:21 +0100

    Projects Page ~ Testing out Cards, and Link to my projects.

--- src/components/navigation/pages/Projects.jsx
+++ src/components/navigation/pages/Projects.jsx
@@ -1,9 +1,34 @@
 import ProjectsImage from "../../../assets/images/projects.jpg";
+import TrafalgarImage from "../../../assets/images/trafalgar-pixel-perfect-cut.png";
 export default function Projects() {
   return (
     <section className='projects'>
       <img className='projects-image' src={ProjectsImage} alt='projects' />
-      <div className='wrapper wrapper--projects'></div>
+      <div className='wrapper wrapper--projects'>
+        <div className='projects-box'>
+          <img
+            className='projects-box__image'
+            src={TrafalgarImage}
+            alt='trafalgar'
+          />
+          <div className='projects-text'>
+            <h2 className='title projects-text__h2'>
+              Trafalgar Medical Company
+            </h2>
+            <p className='paragraph projects-text__p'>
+              Website for Trafalgar Medical Company, using React with
+              TypeScript, SCSS and React Router.
+            </p>
+          </div>
+          <a
+            className='main-link projects-box__a'
+            href='https://github.com/DanielCikora/Trafalgar-ReactJS-App.git'
+            target='_blank'
+          >
+            Link to the Project.
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
