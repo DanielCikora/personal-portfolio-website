8c631b55d2b78b5c41996abd63551e90f4848c6e
Daniel Cikora <d.cikora98@gmail.com>
2024-04-19 15:34:44 +0200

    NPM Update.

--- src/components/projects-preview/ProjectsPreview.jsx
+++ src/components/projects-preview/ProjectsPreview.jsx
@@ -1,55 +1,22 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import MountainImage from "../../assets/images/mountain.jpg";
-import DesertImage from "../../assets/images/desert.jpg";
-import ForestImage from "../../assets/images/forest.jpg";
-import {
-  faCircleArrowLeft,
-  faCircleArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Slider from "./Slider";
 import { Link } from "react-router-dom";
 export default function ProjectsPreview() {
   return (
     <section className='projects-preview'>
-      <div className='wrapper wrapper--projects-preview'>
-        <div className='projects-preview-text'>
-          <h2 className='title projects-preview-text__title'>
-            PREVIEW OF MY PROJECTS
-          </h2>
-          <p className='paragraph projects-preview-text__paragraph'>
-            These are the ...
-          </p>
-          <Link className='cta projects-preview-text__btn' to='/Projects'>
-            Learn more
-          </Link>
-        </div>
-        <div className='projects-preview-slider__wrapper'>
-          <button className='projects-preview-button projects-preview-button--prev'>
-            <FontAwesomeIcon icon={faCircleArrowLeft} />
-          </button>
-          <button className='projects-preview-button projects-preview-button--next'>
-            <FontAwesomeIcon icon={faCircleArrowRight} />
-          </button>
-          <Swiper
-            className='projects-swiper'
-            navigation={{
-              nextEl: ".projects-preview-button--next",
-              prevEl: ".projects-preview-button--prev",
-            }}
-            modules={[Navigation]}
-            slidesPerView={1}
-          >
-            <SwiperSlide>
-              <img className='swiper-slide__image' src={MountainImage} alt='' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className='swiper-slide__image' src={DesertImage} alt='' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className='swiper-slide__image' src={ForestImage} alt='' />
-            </SwiperSlide>
-          </Swiper>
+      <div className='wrapper'>
+        <div className='projects-wrapper'>
+          <div className='projects-preview-text'>
+            <h2 className='title projects-preview-text__title'>
+              PREVIEW OF MY PROJECTS.
+            </h2>
+            <p className='paragraph projects-preview-text__paragraph'>
+              MY PERSONAL PROJECTS.
+            </p>
+            <Link className='cta projects-preview-text__btn' to='/Projects'>
+              Learn more
+            </Link>
+          </div>
+          <Slider />
         </div>
       </div>
     </section>
---
+++ src/components/projects-preview/Slider.jsx
@@ -0,0 +1,47 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import MountainImage from "../../assets/images/mountain.jpg";
+import DesertImage from "../../assets/images/desert.jpg";
+import ForestImage from "../../assets/images/forest.jpg";
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "swiper/css";
+export default function Slider() {
+  return (
+    <>
+      <div className='projects-btn__wrapper'>
+        <button className='projects-preview-btn projects-preview-btn--prev'>
+          <FontAwesomeIcon icon={faCircleArrowLeft} />
+        </button>
+        <button className='projects-preview-btn projects-preview-btn--next'>
+          <FontAwesomeIcon icon={faCircleArrowRight} />
+        </button>
+      </div>
+      <Swiper
+        modules={[Navigation]}
+        slidesPerView={1}
+        navigation={{
+          prevEl: ".projects-preview-btn--prev",
+          nextEl: ".projects-preview-btn--next",
+        }}
+      >
+        <SwiperSlide>
+          <img
+            className='slider-image'
+            src={MountainImage}
+            alt='project-image'
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='slider-image' src={DesertImage} alt='project-image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='slider-image' src={ForestImage} alt='project-image' />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
