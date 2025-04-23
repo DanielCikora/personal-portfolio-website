b9e26e55b72455b73880680f868f48dfca20ddac
Daniel Cikora <d.cikora98@gmail.com>
2025-04-23 18:06:09 +0200

    Constants - FCreated, Projects moved.

--- src/components/navigation/pages/projects/ProjectCards.jsx
+++ src/components/navigation/pages/projects/ProjectCards.jsx
@@ -1,70 +1,12 @@
 import { useState } from "react";
-import ExoticRestaurant from "../../../../assets/images/exotic-restaurant.png";
-import WorkSphereImage from "../../../../assets/images/work-sphere-image.png";
-import ZummpathImage from "../../../../assets/images/zummpath-image.png";
-import PortfolioImage from "../../../../assets/images/portfolio.png";
-import FinanceTrackerImage from "../../../../assets/images/finance-tracker-image.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faWebAwesome } from "@fortawesome/free-brands-svg-icons";
+import { projectCardsContent } from "../../../../lib/constants/constants";
 export default function ProjectCards() {
   const [selectedCard, setSelectedCard] = useState(null);
   const openGallery = (key) => {
     setSelectedCard((prevSelected) => (key === prevSelected ? null : key));
   };
-  const projectCardsContent = [
-    {
-      key: 0,
-      cardImgSrc: FinanceTrackerImage,
-      cardAlt: "project-finance-tracker",
-      cardTitle: "Finance Tracker",
-      cardDescription:
-        "Frontend - Personal Finance Tracker - Next.js, Tailwind.css, TypeScript",
-      cardGitHubLink: "https://github.com/DanielCikora/finance-tracker",
-      cardLink: "https://finance-tracker-seven-delta.vercel.app/dashboard",
-    },
-    {
-      key: 1,
-      cardImgSrc: ExoticRestaurant,
-      cardAlt: "project-exotic-restaurant",
-      cardTitle: "Exotic Indian Eats",
-      cardDescription:
-        "Fullstack - Exotic Indian Restaurant - Next.js, Tailwind.css, Express.js, MongoDB",
-      cardGitHubLink: "https://github.com/DanielCikora/exotic-restaurant",
-      cardLink: "https://exotic-restaurant-tzp1.vercel.app/",
-    },
-    {
-      key: 2,
-      cardImgSrc: ZummpathImage,
-      cardAlt: "project-zummpath-bioinformatics",
-      cardTitle: "Zummpath Bioinformatics",
-      cardDescription:
-        "Frontend - Zummpath Bioinformatics Static Web Page - React.js, GSAP, Three.js, Tailwind.css",
-      cardGitHubLink:
-        "https://github.com/DanielCikora/zummpath-bio-informatics",
-      cardLink: "https://zummpath-bio-informatics.vercel.app/",
-    },
-    {
-      key: 3,
-      cardImgSrc: WorkSphereImage,
-      cardAlt: "project-work-sphere",
-      cardTitle: "Work Sphere",
-      cardDescription:
-        "Frontend - Work Sphere Job Search Web App - Next.js, TypeScript, Redux.js, Three.js, Tailwind.css",
-      cardGitHubLink: "https://github.com/DanielCikora/work-sphere",
-      cardLink: "https://dc-worksphere.netlify.app",
-    },
-    {
-      key: 4,
-      cardImgSrc: PortfolioImage,
-      cardAlt: "project-personal-portfolio",
-      cardTitle: "Personal Portfolio",
-      cardDescription:
-        "Frontend - Personal Portfolio Static Web Page - React.js, React Router, SASS",
-      cardGitHubLink:
-        "https://github.com/DanielCikora/personal-portfolio-website",
-      cardLink: "https://danielcikora.netlify.app",
-    },
-  ];
   return (
     <>
       {projectCardsContent.map((projectCard) => (
---
+++ src/lib/constants/constants.js
@@ -0,0 +1,58 @@
+import ExoticRestaurant from "../../assets/images/exotic-restaurant.png";
+import WorkSphereImage from "../../assets/images/work-sphere-image.png";
+import ZummpathImage from "../../assets/images/zummpath-image.png";
+import PortfolioImage from "../../assets/images/portfolio.png";
+import FinanceTrackerImage from "../../assets/images/finance-tracker-image.png";
+export const projectCardsContent = [
+  {
+    key: 0,
+    cardImgSrc: FinanceTrackerImage,
+    cardAlt: "project-finance-tracker",
+    cardTitle: "Finance Tracker",
+    cardDescription:
+      "Frontend - Personal Finance Tracker - Next.js, Tailwind.css, TypeScript",
+    cardGitHubLink: "https://github.com/DanielCikora/finance-tracker",
+    cardLink: "https://finance-tracker-seven-delta.vercel.app/dashboard",
+  },
+  {
+    key: 1,
+    cardImgSrc: ExoticRestaurant,
+    cardAlt: "project-exotic-restaurant",
+    cardTitle: "Exotic Indian Eats",
+    cardDescription:
+      "Fullstack - Exotic Indian Restaurant - Next.js, Tailwind.css, Express.js, MongoDB",
+    cardGitHubLink: "https://github.com/DanielCikora/exotic-restaurant",
+    cardLink: "https://exotic-restaurant-tzp1.vercel.app/",
+  },
+  {
+    key: 2,
+    cardImgSrc: ZummpathImage,
+    cardAlt: "project-zummpath-bioinformatics",
+    cardTitle: "Zummpath Bioinformatics",
+    cardDescription:
+      "Frontend - Zummpath Bioinformatics Static Web Page - React.js, GSAP, Three.js, Tailwind.css",
+    cardGitHubLink: "https://github.com/DanielCikora/zummpath-bio-informatics",
+    cardLink: "https://zummpath-bio-informatics.vercel.app/",
+  },
+  {
+    key: 3,
+    cardImgSrc: WorkSphereImage,
+    cardAlt: "project-work-sphere",
+    cardTitle: "Work Sphere",
+    cardDescription:
+      "Frontend - Work Sphere Job Search Web App - Next.js, TypeScript, Redux.js, Three.js, Tailwind.css",
+    cardGitHubLink: "https://github.com/DanielCikora/work-sphere",
+    cardLink: "https://dc-worksphere.netlify.app",
+  },
+  {
+    key: 4,
+    cardImgSrc: PortfolioImage,
+    cardAlt: "project-personal-portfolio",
+    cardTitle: "Personal Portfolio",
+    cardDescription:
+      "Frontend - Personal Portfolio Static Web Page - React.js, React Router, SASS",
+    cardGitHubLink:
+      "https://github.com/DanielCikora/personal-portfolio-website",
+    cardLink: "https://danielcikora.netlify.app",
+  },
+];
